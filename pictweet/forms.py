9199cea8190b3071bbeac766c1676f74a57c6a0b
Takabun <takafumi.kido@gmail.com>
2019-07-10 20:27:12 +0900

    投稿機能完了

--- pictweet/forms.py
+++ pictweet/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from .models import User
+<<<<<<< Updated upstream
 
 class TweetForm(forms.Form):
     text = forms.CharField(
@@ -14,6 +15,19 @@ class TweetForm(forms.Form):
     image = forms.ImageField(
     # upload_to='pictweet/images/'
     )
+=======
+from django.contrib.auth import forms as auth_forms
+from .models import Tweet #modelform導入につき
+
+
+class TweetForm(forms.ModelForm):
+    """投稿画面用のフォーム"""
+    class Meta:
+        #利用するモデルクラスを設定
+        model = Tweet
+        #利用するモデルのフィールドを設定
+        fields = ('text', 'image')
+>>>>>>> Stashed changes
 
 # date_time = models.DateTimeField()
 # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
