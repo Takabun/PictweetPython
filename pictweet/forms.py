9f3e0f416205c4496f3d1e4f5581d2677b9bc068
Takabun <takafumi.kido@gmail.com>
2019-07-10 20:33:36 +0900

    不要なビュー(create)削除

--- pictweet/forms.py
+++ pictweet/forms.py
@@ -1,21 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import AuthenticationForm
 from .models import User
-<<<<<<< Updated upstream
-
-class TweetForm(forms.Form):
-    text = forms.CharField(
-        max_length=140,
-        required=True,
-        # blank=False
-    )
-#↑TextFieldにするとエラー。現状、modelとは齟齬がある状態
-
-    image = forms.ImageField(
-    # upload_to='pictweet/images/'
-    )
-=======
 from django.contrib.auth import forms as auth_forms
 from .models import Tweet #modelform導入につき
 
@@ -27,30 +13,15 @@ class TweetForm(forms.ModelForm):
         model = Tweet
         #利用するモデルのフィールドを設定
         fields = ('text', 'image')
->>>>>>> Stashed changes
 
 # date_time = models.DateTimeField()
 # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 # like_count = models.IntegerField(default=0)
 
 
-#新規登録
-class UserCreateForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        #htmlの表示を変更可能にします
-        self.fields['nickname'].widget.attrs['class'] = 'form-control'
-        self.fields['email'].widget.attrs['class'] = 'form-control'
-        self.fields['password'].widget.attrs['class'] = 'form-control'
-
-    class Meta:
-       model = User
-       fields = ("nickname", "email", "password",)
-
-#ログイン
-class LoginForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-       super().__init__(*args, **kwargs)
-       #htmlの表示を変更可能にします
-       self.fields['nickname'].widget.attrs['class'] = 'form-control'
-       self.fields['password'].widget.attrs['class'] = 'form-control'
+class LoginForm(auth_forms.AuthenticationForm):
+    '''ログインフォーム'''
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        for field in self.fields.values():
+            field.widget.attrs['placeholder'] = field.label
--- pictweet/urls.py
+++ pictweet/urls.py
@@ -3,15 +3,15 @@ from django.urls import path
 from . import views
 #ログアウト
 # from django.contrib.auth.views import logout
-
+app_name = 'pictweet'
 urlpatterns = [
     path('', views.index, name='index'),
     path('new/', views.new, name='new'),
-    path('create/', views.create, name='create'),
     path('user/', views.user, name='user'),
     # path('logout/', logout, {'template_name': 'index.html'}, name='logout'), 
     # path('logout/', logout, {'template_name': 'index.html'}, name='logout'), 
-    path('createuser/', views.Create_Account, name='createuser'),
-    path('login/', views.Account_login, name='login'),
-    path('logout/', views.index, name='logout'),
+    path('login/', views.loginView.as_view(), name="login"),
+    path('logout/', views.logoutView.as_view(), name="logout"),
+    path('createuser/', views.createView.as_view(),name="create"), # 追記
+
 ]
